e349362963a65faa8af59237db3f5773ec68f1f6
Atharva Gaikwad <atharvagaikwad125@gmail.com>
2026-04-13 21:02:55 +0530

    first commit

---
+++ GigWorks-API/gigworks/src/main/java/com/gigworks/gigworks/GigworksApplication.java
@@ -0,0 +1,13 @@
+package com.gigworks.gigworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GigworksApplication.class, args);
+    }
+
+}
---
+++ GigWorks-API/gigworks/src/test/java/com/gigworks/gigworks/GigworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigworks.gigworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
